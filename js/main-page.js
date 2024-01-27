9ab0950ba3fdbfaa7220e09f26d9d345734f404a
Siqius <86854646+Siqius@users.noreply.github.com>
2024-01-27 23:32:21 +0100

    get players ironman profiles

--- js/main-page.js
+++ js/main-page.js
@@ -1,10 +1,10 @@
 const input = document.querySelector('#input');
 const button = document.querySelector('#searchButton');
-async function fetchData(fetchUrl, sendMethod ,objectToSend) {
+async function fetchData(fetchUrl, sendMethod) {
   try {
       let response = await fetch(fetchUrl, {
           method: sendMethod,
-          body: objectToSend === "null" ? null : JSON.stringify(objectToSend),
+          
       });
 
       if (!response.ok) {
@@ -12,7 +12,7 @@ async function fetchData(fetchUrl, sendMethod ,objectToSend) {
       }
 
       let result = await response.json();
-      return result.uuid;
+      return result;
 
   } catch (error) {
       console.error('There was a problem with the fetch operation:', error);
@@ -22,6 +22,19 @@ async function fetchData(fetchUrl, sendMethod ,objectToSend) {
 async function searchForPlayer() {
   const playerName = input.value;
   const mojangURL = `https://api.ashcon.app/mojang/v2/user/${playerName}`;
-  const playerUUID = await fetchData(mojangURL, 'GET', "null");
-  console.log(playerUUID);
+  const playerUUID = await fetchData(mojangURL, 'GET');
+  console.log(playerUUID.uuid)
+  
+  let playerInfo = await fetchData("https://api.hypixel.net/v2/skyblock/profiles?uuid=" + playerUUID.uuid + "&key=2d65dd58-a376-4852-bbdb-5957d2d0d1c6", 'GET');
+  playerInfo = playerInfo.profiles;
+  let ironmanProfiles = [];
+  for(let i = 0;i<playerInfo.length; i++) {
+    if(playerInfo[i].game_mode == "ironman") {
+      ironmanProfiles.push(playerInfo[i]);
+    }
+  }
+  if(ironmanProfiles.length == 0) {
+    document.getElementById("ironmanProfiles").innerHTML = "No Ironman Profiles Found";
+    return;
+  }
 }
